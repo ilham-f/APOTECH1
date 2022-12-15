0989686aa6785e9bc0fae23fba22d78bd0d8cadf
Moh. Ilham Fahrezy <mifahrezy11@gmail.com>
2022-12-15 16:25:35 +0700

    commit 26
    Update UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class UserController extends Controller
 {
